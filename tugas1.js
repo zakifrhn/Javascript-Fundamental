299fc3f865966a0c0140f55b1452bd0c232e2b33
zakifrhn <zaki.farhan026@gmail.com>
2023-05-30 19:46:04 +0700

    Biodata menggunakan array of object

---
+++ tugas1.js
@@ -0,0 +1,47 @@
+const biodata ={
+    name: "Zaki",
+    age: 22,
+    hobbies: ['badminton', 'futsal'],
+    isMarried: false,
+    schollList: [
+        {
+            name: 'SDN 010 Pancoran Pagi',
+            yearsIn: 2005,
+            yearsOut: 2011,
+            major: null
+        },
+        {
+            name: 'SMPN 141 Jakarta',
+            yearsIn: 2011,
+            yearsOut: 2014,
+            major: null
+        },
+        {
+            name: 'SMAN 43 Jakarta',
+            yearsIn: 2014,
+            yearsOut: 2017,
+            major: 'IPA'
+        },
+        {
+            name: 'Universitas Muhammadiyah Prof. Dr Hamka',
+            yearsIn: 2018,
+            yearsOut: 2023,
+            major: 'Teknik Informatika'
+        }],
+    Skills: [
+        {
+            skillName: 'SQL',
+            level: 'beginner'
+        },
+        {
+            skillName: 'phrase of word',
+            level: 'advanced'
+        },
+        {
+            skillName: 'Design with HTML + CSS',
+            level: 'beginner'
+        }],
+    interesInCoding: true
+}
+
+console.log(biodata)
